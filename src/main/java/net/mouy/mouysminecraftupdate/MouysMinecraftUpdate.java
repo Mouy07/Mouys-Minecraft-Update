47d4c9d995d60a4e020bf0ca2786e2a6b98f5c3b
Mouy07 <88009582+Mouy07@users.noreply.github.com>
2023-01-30 14:52:50 -0600

    Initial commit

---
+++ src/main/java/net/mouy/mouysminecraftupdate/MouysMinecraftUpdate.java
@@ -0,0 +1,15 @@
+package net.mouy.mouysminecraftupdate;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MouysMinecraftUpdate implements ModInitializer {
+	public static final String MOD_ID = "mouysminecraftupdate";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/mouy/mouysminecraftupdate/MouysMinecraftUpdateClient.java
@@ -0,0 +1,10 @@
+package net.mouy.mouysminecraftupdate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MouysMinecraftUpdateClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
